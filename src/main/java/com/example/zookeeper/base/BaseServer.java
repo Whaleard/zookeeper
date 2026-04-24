1e29ce41422775332a70ddb82a7a939bc4f43333
chengmengd <chengmengd@isoftstone.com>
2026-04-24 23:09:42 +0800

    功能性基类

---
+++ src/main/java/com/example/zookeeper/base/BaseServer.java
@@ -0,0 +1,9 @@
+package com.example.zookeeper.base;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class BaseServer {
+
+    protected final Logger logger = LogManager.getLogger(this.getClass());
+}
